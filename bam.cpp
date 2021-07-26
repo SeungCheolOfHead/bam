17681e330ec3496b865060342e2eaed155835239
jsc9475 <jsc9475@naver.com>
2021-07-26 22:25:37 +0900

    bam day 2

--- bam.cpp
+++ bam.cpp
@@ -1,30 +1,52 @@
 #include "bam.h"
 
+//enum selectnumber1
+//{
+//	a = 0,
+//	gamestart = 0,
+//};
+// 한번에 주석 : ctrl + k + c , 주석풀기 : ctrl + k + u
+// 
+// 
+// 더블버퍼링 <- cls
+
+// 도서관리, node (c++ 연결리스트), (c++ 클래스 <--), (vector, list),  구글에 c++ stl    
+
 int main(void)
 {
 	consoleSize();
 	while (1) {
 		titleDraw();
 		int menuSelect = menu();
-		if (menuSelect == 0)		//게임시작
+		switch (menuSelect)
 		{
-			int startmenu = gameStart();
-			if (startmenu == 0)
+			case 0:
 			{
-
+				int startmenu = gameStart();
+				if (startmenu == 0)
+				{
+					int diff = difficulty();
+					switch (diff)
+					{
+					case 0:
+						break;
+					case 1:
+						break;
+					case 2:
+						break;
+					}
+				}
+				break; 
 			}
-			else if (startmenu == 4)
+			case 1:
 			{
+				gameInfo();
 				break;
 			}
-		}
-		else if (menuSelect == 1)	//게임정보
-		{
-			gameInfo();
-		}
-		else if (menuSelect == 2)	//게임종료
-		{
-			return 0;
+			case 2:
+			{
+				return 0;
+			}
 		}
 		system("cls");
 	}
--- bam.h
+++ bam.h
@@ -11,6 +11,7 @@ void gotoxy(int x, int y);
 int menu();
 void gameInfo();
 int gameStart();
+int difficulty();
 
 enum {
 	UP = 72,
@@ -48,6 +49,11 @@ void gotoxy(int x, int y)
 
 int control()
 {
+	/*if (GetAsyncKeyState(VK_LEFT) < 0)
+	{
+
+	}*/
+
 	char temp = _getch();
 
 	if (temp == '72')
@@ -74,8 +80,8 @@ int menu()
 	printf("게 임 종 료\n");
 	while (1)
 	{
-		int n = control();
-		switch (n) 
+		int menuSelect = control();
+		switch (menuSelect)
 		{
 			case UP:			//UP
 				if (y > 13)
@@ -103,7 +109,7 @@ void gameInfo()
 {
 	system("cls");
 	printf("\n\n\n");
-	printf("                         뱀게임에 오신 것을 환영합니다\n\n\n\n");
+	printf("                          뱀게임에 오신 것을 환영합니다\n\n\n\n");
 	printf("                                      조작법\n\n");
 	printf("                          이동 : 방향키를 사용합니다. \n");
 	printf("                          선택 : 엔터키를 사용합니다. \n");
@@ -118,32 +124,83 @@ int gameStart()
 {
 	int x = 35;
 	int y = 13;
+
+	const int xStep = 2;
+	const int yStep = 2;
+
 	system("cls");
-	gotoxy(x - 2, y);
+	gotoxy(x - xStep, y);
 	printf("> 게임 난이도\n");
-	gotoxy(x, y + 4);
+	gotoxy(x, y + yStep);
 	printf("뒤로가기");
 	while (1)
 	{
-		int n = control();
-		switch (n)
+		int difficultySelect = control();
+		switch (difficultySelect)
+		{
+		case UP:			//UP
+			if (y > 13)
+			{
+				gotoxy(x - xStep, y);
+				printf(" ");
+				gotoxy(x - xStep, y -= yStep);
+				printf(">");
+			}break;
+		case DOWN:			//DOWN
+			if (y < 15)
+			{
+				gotoxy(x - xStep, y);
+				printf(" ");
+				gotoxy(x - xStep, y += yStep);
+				printf(">");
+			}break;
+		case ENTER:			//ENTER
+			if (y == 13)
+			{
+				gotoxy(x - xStep, y);
+				printf(" ");
+			}
+			return y - 13;
+		}
+	}
+}
+
+int difficulty()
+{
+	int x = 49;
+	int y = 13;
+	gotoxy(x - 2, y);
+	printf("> 쉬움\n");
+	gotoxy(x, y + 1);
+	printf("보통");
+	gotoxy(x, y + 2);
+	printf("어려움");
+	while (1)
+	{
+		int difficultymenu = control();
+		switch (difficultymenu)
 		{
 		case UP:			//UP
 			if (y > 13)
 			{
 				gotoxy(x - 2, y);
 				printf(" ");
-				gotoxy(x - 2, y - 4);
+				gotoxy(x - 2, --y);
 				printf(">");
 			}break;
 		case DOWN:			//DOWN
-			if (y < 17)
+			if (y < 15)
 			{
 				gotoxy(x - 2, y);
 				printf(" ");
-				gotoxy(x - 2, y + 4);
+				gotoxy(x - 2, ++y);
 				printf(">");
 			}break;
+		case LEFT:			//LEFT
+			gameStart();
+			if (x == 49 || y == 13) {
+				difficulty();
+			}
 		case ENTER:			//ENTER
 			return y - 13;
 		}
